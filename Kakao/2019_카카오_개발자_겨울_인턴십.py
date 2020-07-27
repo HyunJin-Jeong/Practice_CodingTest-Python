0e6de66579929a942cb5d74c4e4c6c7d39c75e2b
hyunJin <hyun0524e@naver.com>
2020-07-27 19:21:02 +0900

    add

--- Kakao/2019_카카오_개발자_겨울_인턴십.py
+++ Kakao/2019_카카오_개발자_겨울_인턴십.py
@@ -1,19 +1,12 @@
 def solution(board, moves):
-    answer = 0
-    bucket = []
+    answer = 0; bucket = []
     for i in moves:
-        for j in range(len(board)):
+        for j in range(0, len(board)):
             if board[j][i - 1] != 0:
                 bucket.append(board[j][i - 1])
                 board[j][i - 1] = 0
                 if len(bucket) > 1 and bucket[-1] == bucket[-2]:
-                    del bucket[-1]
-                    del bucket[-1]
+                    del bucket[-2:]
                     answer += 2
                 break
     return answer
-
-test = [1,2,3,4,5]
-print(test[:-2])
-print(test[-1:-2])
-print(test[-2:])
