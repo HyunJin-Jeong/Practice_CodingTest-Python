540f13d4f937113bf2ee45c64e92215b4ec10960
hyunJin <hyun0524e@naver.com>
2020-07-28 19:21:12 +0900

    Summer_Winter_Coding(~2018)_2

--- etc/Summer_Winter_Coding(~2018).py
+++ etc/Summer_Winter_Coding(~2018).py
@@ -1,13 +1,18 @@
 def solution(skill, skill_trees):
     answer = 0
-    answer = ["hi" for i in skill_trees if skill in i]
 
-    '''
-    for i in skill_trees:
-        if skill in i:
+    for tree in skill_trees:
+        count = 0
+        tmp = 0
+
+        for i in skill:
+            if i in tree[tmp:]:
+                tmp = tree.find(i)
+                count += 1
+
+        if count == len(skill):
             answer += 1
-    '''
+
     return answer
-
-
+    
 print(solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"]))
