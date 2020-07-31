bb4cef0e3f53e4121e38acfcd7827af44d92f4d3
HyunJin-Jeong <hyun0524e@naver.com>
2020-07-31 22:45:17 +0900

    Summer&Winter_Coding(2019)-foldpaper_v

--- etc/Summer&Winter_Coding(2019)-종이접기.py
+++ etc/Summer&Winter_Coding(2019)-종이접기.py
@@ -1,8 +1,8 @@
 def solution(n):
-    fold = 0
-    answer = [fold]
+    v = 0
+    answer = [v]
 
     for i in range(n - 1):
-        answer = answer + [fold] + [sw ^ 1 for sw in answer[::-1]]
+        answer = answer + [v] + [sw ^ 1 for sw in answer[::-1]]
 
     return answer
