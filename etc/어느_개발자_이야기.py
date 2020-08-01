043a484df6fa2896f7b7bcebd665967ebd2ff3fb
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-01 13:27:31 +0900

    어느_개발자_이야기_ing_1

--- etc/어느_개발자_이야기.py
+++ etc/어느_개발자_이야기.py
@@ -1,12 +1,13 @@
 def solution(num):
-    for i in range(2,99):
-        for j in range(2,99):
-            tmp = str(j ** 2)
-            try:
-                if num == int(tmp, i):
-                    return i
-            except ValueError as err:
-                pass
-                
+    print(int(str(num), 10) ** 0.5)
+
+    '''
+    for i in range(2, 99):
+        try:
+            if int(str(num), 10) ** 0.5:
+                print(i)
+        except ValueError as err:
+            pass
+    '''
 print(solution(15))
 #N진법으로 표현하는 문법 = int(num, base)
