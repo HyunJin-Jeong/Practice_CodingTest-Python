fee300be9839d4db2754152b359989c378922bb7
hyunJin <hyun0524e@naver.com>
2020-08-03 17:21:55 +0900

    remove_blank

--- etc/2020_카카오_블라인드_채용-문자열압축.py
+++ etc/2020_카카오_블라인드_채용-문자열압축.py
@@ -1,28 +1,21 @@
 def solution(s):
     min = float('inf')
     cnt = 1
-
     for i in range(1, len(s)+1):
         tmp = s[0:i]
         comp = ''
-
         for j in range(i, len(s)+1, i):
             if tmp == s[j:j+i]:
                 cnt += 1
-
             elif cnt == 1:
                 comp += tmp
                 tmp = s[j:j+i]
-
             else:
                 comp += str(cnt) + tmp
                 tmp = s[j:j+i]
                 cnt = 1
-
         if len(s) % i != 0:
             comp += s[-(len(s) % i):]
-
         if min > len(comp):
             min = len(comp)
-
     return min
