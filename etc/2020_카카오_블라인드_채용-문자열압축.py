ee1bf83210a2aa050779335b8dd9b12732ced0b9
hyunJin <hyun0524e@naver.com>
2020-08-03 16:53:27 +0900

    compress_string2

--- etc/2020_카카오_블라인드_채용-문자열압축.py
+++ etc/2020_카카오_블라인드_채용-문자열압축.py
@@ -1,19 +1,24 @@
 def solution(s):
-    answer = ''
-
-    for i in range(len(s)):
-        cnt = 1
+    min = float('inf')
+    
+    for i in range(1, len(s)+1):
         tmp = s[0:i]
-        for j in range(i+1, len(s), i+1):
+        comp = ''
+        cnt = 1
+
+        for j in range(i, len(s)+1, i):
             if tmp == s[j:j+i]:
                 cnt += 1
+
+            elif cnt == 1:
+                comp += tmp
+
             else:
-                answer += str(cnt) + tmp
+                comp += str(cnt) + tmp
+                tmp = s[j:j+i]
                 cnt = 1
 
-    return answer
+        if min > len(comp):
+            min = len(comp)
 
-i = 1
-s[0:1] = ab
-ab == s[0:0+1] == ab? yes cnt +1
-ab == s[j+i=:] == ba? no answer += 2ab
+    return min
