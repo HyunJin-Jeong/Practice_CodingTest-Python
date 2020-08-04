6bbd3cbc27042b8a897f763b0e8dd0b24c4d1227
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-05 00:13:04 +0900

    complete_player_finish

--- etc/완주하지_못한_선수.py
+++ etc/완주하지_못한_선수.py
@@ -8,7 +8,9 @@ def solution(participant, completion):
         if i in completion:
             answer.remove(i); completion.remove(i)
 
-    return answer
+    return ''.join(answer)
+
+    #return [answer.remove(i), completion.remove(i) for i in participant if i in completion]
 
 print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
 print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
