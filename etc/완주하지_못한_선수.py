69a2d80716eb62e6f7f66b6b81015b6a64e970bd
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-05 23:24:03 +0900

    complete_player_comprehensions

--- etc/완주하지_못한_선수.py
+++ etc/완주하지_못한_선수.py
@@ -3,14 +3,8 @@ import copy
 def solution(participant, completion):
 
     answer = copy.deepcopy(participant)
-
-    for i in participant:
-        if i in completion:
-            answer.remove(i); completion.remove(i)
-
+    [(answer.remove(i), completion.remove(i)) for i in participant if i in completion]
     return ''.join(answer)
-
-    #return [answer.remove(i), completion.remove(i) for i in participant if i in completion]
 
 print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
 print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
