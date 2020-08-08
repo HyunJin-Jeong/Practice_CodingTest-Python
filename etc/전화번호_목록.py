08ea7d2bd29f3f5549077737dc72f81f597d8237
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-08 20:42:02 +0900

    phone_book_list_addother

--- etc/전화번호_목록.py
+++ etc/전화번호_목록.py
@@ -6,6 +6,14 @@ def solution(phone_book):
                 return False
     return True
 
+'''
+def solution(phone_book):
+    phone_book = sorted(phone_book)
+    for p1, p2 in zip(phone_book, phone_book[1:]):
+        print(p1, p2)
+    return True
+'''
+
 print(solution(['119','97674223','1195524421']))
 print(solution(['123','456','789']))
 print(solution(['12','123','1235','567','88']))
