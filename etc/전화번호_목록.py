ce484840b2c4e817126a8070f36bef246ee04f8e
HyunJin-Jeong <hyun0524e@naver.com>
2020-08-08 20:30:07 +0900

    phone_book_list_finish

--- etc/전화번호_목록.py
+++ etc/전화번호_목록.py
@@ -1,10 +1,10 @@
 def solution(phone_book):
-    for i in range(len(phone_book)-1):
-        for j in range(len(phone_book)):
-            if phone_book[i] in phone_book[j] and i != j:
+    leng = len(phone_book)
+    for i in range(leng):
+        for j in range(leng):
+            if phone_book[i] in phone_book[j][:len(phone_book[i])] and i != j:
                 return False
     return True
-
 
 print(solution(['119','97674223','1195524421']))
 print(solution(['123','456','789']))
